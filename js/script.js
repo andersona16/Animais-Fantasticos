37b15de0a0d4053885547734ee89ca8191b02665
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-06-01 18:25:22 -0300

    Finalizado HTML e CSS

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const hrefPagina = window.location.href;
+console.log(hrefPagina)
+
+
+const elementoAtivo = document.querySelector('.ativo')
+
+
+const linguagem = window.navigator.language;
+console.log(linguagem)
+
+const windowWidth = window.innerWidth;
+console.log(windowWidth)
