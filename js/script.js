ddf12a9eb066c7bd55e37cd888b0aafb1d5f5b83
Anderson Araujo <andersonaraujoc1@gmail.com>
2022-06-02 23:18:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-const animais = document.getElementById('animais')
-console.log(animais)
+// const linksInternos = document.querySelectorAll('a[href^="#"]')
 
-const gridSection = document.getElementsByClassName('grid-section')
-console.log(gridSection[0])
+// function handleLink(event) {
+//     event.preventDefault()
+//     linksInternos.forEach((link) => {
+//         link.classList.remove('ativo')
+//     });
+//     event.currentTarget.classList.add('ativo');
 
+// }
+
+// linksInternos.forEach((link) => {
+//     link.addEventListener('click', handleLink);
+// })
+
+// function handleElemento(event) {
+//     (event.currentTarget.remove())
+// }
+
+
+// const todosElementos = document.querySelectorAll('body *')
+
+// todosElementos.forEach((elemento) => {
+//     elemento.addEventListener('click', handleElemento)
+// })
+
+// console.log(todosElementos)
+
+
+function handleClickT(event) {
+    console.log(event.key)
+    if (event.key === 't') {
+        console.log('clicou no T')
+    }
+}
+
+window.addEventListener('keydown', handleClickT)
